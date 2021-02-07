3a965aa78a6cd116132a7d8fe47a52cea132b4cd
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2021-02-07 19:28:29 +0100

    website look begun

--- JS/startup.js
+++ JS/startup.js
@@ -1,9 +1,13 @@
 var user;
+var charactersgotten = 0;
+var weaponcounter = 0;
+var data_is_ready = false;
 
-function getItemData(hashId, i, j, Tablename){
-    //console.log(character);
+//instanced weapon perks
+function getItemPerk(hashId, character, j, i, Tablename){
+    //console.log(j);
     var data = new FormData();
-    data.append("Table", Tablename);
+    data.append("tablename", Tablename);
     data.append("itemData",hashId);
     data.append("key", apiKey);
 
@@ -11,21 +15,108 @@ function getItemData(hashId, i, j, Tablename){
     xhr.open("POST", "../PHP/destiny2Manifest.php");
     xhr.onload = function(){
         var json = JSON.parse(this.responseText);
-        //console.log(this.responseText);
-        i.gear[j] = json;
+        //console.log(json);
+        character.gear[j].perks[i] = json;
     }
     xhr.send(data);
 }
-function getItemInfo(ID, type, itemInstanceId){
+
+//weapon stats 2 from static weapon
+function getItemStats2(hashId, character, j, i, Tablename, value){
+    //console.log(hashId);
+    var data = new FormData();
+    data.append("tablename", Tablename);
+    data.append("itemData",hashId);
+    data.append("key", apiKey);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../PHP/destiny2Manifest.php");
+    xhr.onload = function(){
+        var json = JSON.parse(this.responseText);
+        //console.log(json);
+        character.gear[j].staticstats[i] = {
+            description: json.displayProperties.description,
+            name: json.displayProperties.name,
+            hash: json.hash,
+            value: value,   
+        };
+    }
+    xhr.send(data);
+}
+
+//precomputed instanced weapon stats
+function getItemStats(hashId, character, j, i, Tablename, value){
+    //console.log(hashId);
+    var data = new FormData();
+    data.append("tablename", Tablename);
+    data.append("itemData",hashId);
+    data.append("key", apiKey);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../PHP/destiny2Manifest.php");
+    xhr.onload = function(){
+        var json = JSON.parse(this.responseText);
+        //console.log(json);
+        character.gear[j].stats[i] = {
+            description: json.displayProperties.description,
+            name: json.displayProperties.name,
+            hash: json.hash,
+            value: value,
+        };
+    }
+    xhr.send(data);
+}
+
+
+//static data about weapon like name
+function getItemData(hashId, character, j,  Tablename){
+    //console.log(hashId);
+    var data = new FormData();
+    data.append("tablename", Tablename);
+    data.append("itemData",hashId);
+    data.append("key", apiKey);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../PHP/destiny2Manifest.php");
+    xhr.onload = function(){
+        //console.log(this.responseText)
+        var json = JSON.parse(this.responseText);
+        //character.gear[j] = json;
+        character.gear[j].static = json;
+
+        weaponcounter++;
+
+        var stats = Object.keys(json.stats.stats);
+        //console.log(stats)
+        for(var i = 0; i < stats.length; i++){
+           getItemStats2(stats[i], character,j,i,'DestinyStatDefinition', json.stats.stats[stats[i]].value);
+        }
+    }
+    xhr.send(data);
+}
+
+function getItemInfo(ID, type, itemInstanceId, character, j){
     var xhr = new XMLHttpRequest();
     
-    xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/" + type + "/Profile/" + ID + "/Item" + itemInstanceId + "/?components=302, 300, 304", true);
+    xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/" + type + "/Profile/" + ID + "/Item/" + itemInstanceId + "/?components=302, 300, 304", true);
     xhr.setRequestHeader("X-API-Key", apiKey);
     xhr.onreadystatechange = function(){
         
         if(this.readyState === 4 && this.status === 200){
             var json = JSON.parse(this.responseText);
-            console.log(json);
+            //console.log(json);
+            var stats = Object.keys(json.Response.stats.data.stats);
+            
+            for(var i = 0; i < json.Response.perks.data.perks.length; i++){
+                
+                getItemPerk(json.Response.perks.data.perks[i].perkHash, character,j,i,  'DestinySandboxPerkDefinition');
+                if(i < stats.length){
+                    //console.log(stats[i])
+                    getItemStats(stats[i], character,j,i,'DestinyStatDefinition', json.Response.stats.data.stats[stats[i]].value);
+                }
+            }
+
+            
         }
     }
     xhr.send();
@@ -50,10 +141,19 @@ function getCharacterInfo(ID, type, characterId, i){
                     gear : [],
             };
             user.characters[i] = character;
+            weaponcounter = 0;
+            charactersgotten++;
             for(var j = 0; j < json.Response.equipment.data.items.length; j++){
+
+                user.characters[i].gear[j] = {
+                    staticstats: [],
+                    stats: [],
+                    perks: [],
+                    static: "",
+                };
                 
-                getItemData(json.Response.equipment.data.items[j].itemHash, user.characters[i], j, "DestinyInventoryItemDefinition");
-                getItemInfo(ID, type, json.Response.equipment.data.items[0].itemInstanceId);
+                getItemInfo(ID, type, json.Response.equipment.data.items[0].itemInstanceId, user.characters[i], j);
+                getItemData(json.Response.equipment.data.items[j].itemHash, user.characters[i], j, 'DestinyInventoryItemDefinition');
                 //character.gear[j] = json.Response.equipment.data.items[j].itemHash;
             }
             //console.log(characters);
@@ -116,6 +216,8 @@ function createUser() {
             //user = new User(users[0].displayName, users[0].membershipId, users[0].membershipType);
             getCharacters(users[0].membershipId, users[0].membershipType);
             console.log(user);
+            data_is_ready = true;
+
         }
     }
     xhr.send();
